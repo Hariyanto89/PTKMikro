c1f28d43099bc046cb1018a5dae3b4b5dd4ab2d3
Hariyanto <yantonft@gmail.com>
2024-11-15 09:10:44 +0700

    Update server.js
    Signed-off-by: Hariyanto <yantonft@gmail.com>

--- server.js
+++ server.js
@@ -1,55 +1,52 @@
 const express = require('express');
-const fs = require('fs-extra');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
+// Middleware untuk mengakses JSON dan file statis
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'assets')));
 
-// Lokasi file JSON
-const DATA_FILE = './data/employees.json';
-
-// Fungsi untuk membaca data dari file JSON
-async function readEmployees() {
-    try {
-        const data = await fs.readFile(DATA_FILE, 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        return [];
-    }
-}
-
-// Fungsi untuk menulis data ke file JSON
-async function writeEmployees(data) {
-    try {
-        await fs.writeFile(DATA_FILE, JSON.stringify(data, null, 2));
-        return true;
-    } catch (error) {
-        console.error('Gagal menyimpan data:', error);
-        return false;
-    }
-}
-
-// Endpoint untuk mengambil data pegawai
-app.get('/api/employees', async (req, res) => {
-    const employees = await readEmployees();
-    res.json(employees);
+// Endpoint untuk mendapatkan data pegawai
+app.get('/api/employees', (req, res) => {
+    const dataPath = path.join(__dirname, 'data', 'employees.json');
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error membaca data pegawai');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
-// Endpoint untuk menambah data pegawai
-app.post('/api/employees', async (req, res) => {
+// Endpoint untuk menambahkan pegawai baru ke file JSON
+app.post('/api/employees', (req, res) => {
+    const dataPath = path.join(__dirname, 'data', 'employees.json');
     const newEmployee = req.body;
-    const employees = await readEmployees();
-    employees.push(newEmployee);
 
-    const success = await writeEmployees(employees);
-    if (success) {
-        res.status(201).json({ message: 'Pegawai berhasil ditambahkan' });
-    } else {
-        res.status(500).json({ message: 'Gagal menyimpan data pegawai' });
-    }
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error membaca data pegawai');
+        } else {
+            const employees = JSON.parse(data);
+            employees.push(newEmployee);
+
+            fs.writeFile(dataPath, JSON.stringify(employees, null, 2), (err) => {
+                if (err) {
+                    res.status(500).send('Error menambahkan pegawai');
+                } else {
+                    res.status(201).send('Pegawai berhasil ditambahkan');
+                }
+            });
+        }
+    });
+});
+
+// Rute untuk mengembalikan komponen HTML dari folder components
+app.get('/components/:component', (req, res) => {
+    const componentPath = path.join(__dirname, 'components', `${req.params.component}.html`);
+    res.sendFile(componentPath);
 });
 
 // Jalankan server
